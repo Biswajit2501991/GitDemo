cc52525cbd8df7040850e63307569793b8cea527
Biswajit2501991 <biswajitkumar01@gmail.com>
2020-10-03 12:11:16 +0530

    Architech Asian devlopment

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -15,16 +15,49 @@ public class Greeter {
   //TODO: Add javadoc comment
   public String greet(String someone) {
     return String.format("Hello How are you, %s!", someone);
+    return String.format("Web Automation", someone);
     
     
   }
   
   public void postJira()
   {
-	  System.out.println("PostJira");
-	  System.out.println("PostJire2");
-	  System.out.println("PostJire3");
+	 
 	  // pulled from git 
+=======
+	  
+	
+>>>>>>> 8c6e9511a5d66ee3f00f4f10ea7331706249e620
 	  
   }
+ 
+  
+  public void postJira()
+  {
+	  System.out.println("Element ");
+	  System.out.println("Eleene t");
+	  System.out.println("element ");
+  }
+  
+  public void postJira()
+  {
+	  System.out.println("Branch1 ");
+	  System.out.println("Branching t");
+	  System.out.println("Branching 3 ");
+  }
+  
+  public void postJira()
+  {
+	  System.out.println("Branching  4");
+	  System.out.println("Branching 5t");
+	  System.out.println("Branching 6");
+  }
+  
+  public void postJira()
+  {
+	  System.out.println("Element 7");
+	  System.out.println("Eleene t7");
+	  System.out.println("element 9");
+  }
+  
 }
